22ee8caf4b39838d80582c3789118657b8cf0e66
ArunkumarBejagam <arun.bejagam@gmail.com>
2022-01-18 06:26:28 +0800

    Create hurdle4.py

---
+++ reeborg/hurdle4.py
@@ -0,0 +1,27 @@
+## Not efficient but works with less available conditions :
+
+#def jump():
+#    while not at_goal():
+#        if front_is_clear():
+#            move()
+#            turn_right()
+#        else:
+#            turn_left()
+#jump()
+
+
+## Efficient methode.
+def jump():
+    while not at_goal():
+        if wall_in_front() and wall_on_right():
+            turn_left()
+        elif front_is_clear() and wall_on_right(): 
+            move()
+        #elif front_is_clear() and wall_on_right() == False:
+        else:
+            turn_right()
+            move()
+            turn_right()
+            move()
+            
+jump()
